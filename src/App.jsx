92a8b4bcdee2b5b156da92846c1d1415081670b2
VITAL070702 <yvitalraquel@gmail.com>
2025-04-01 18:50:39 -0700

    agregue a AnimalsShow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import{useState} from 'react'
+
 import './App.css'
 
-function App() {
+import AnimalsShow from './AnimalsShow'
+
+function getRandomAnimal(){
+  const animals =['cat','bird','dog','gator','horse']
+  return animals[Math.floor(Math.random()* animals.length)]
+}
+console.log(getRandomAnimal())
+function App(){
+  const[animals, setAnimals]= useState()
+  const handleClick=()=>{
+    setAnimals([...animals, getRandomAnimal()])
+  }
+  const renderAnimals =animals.map((animal, index)=>{
+    return <AnimalsShow type={animal} key ={index}/>
+  })
+
 
   return (
     <>
-    <h1>ANimals app</h1>
+    <h1>Animals app</h1>
     </>
   )
 }
